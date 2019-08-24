31708f83b90eb1689affdf62fc2dc8a2555b5a03
sainivinit <vinitsaini357@gmail.com>
2019-08-24 18:09:27 +0530

    massegeDilogBox

---
+++ src/massegeDilogBox/firstclass.java
@@ -0,0 +1,9 @@
+package massegeDilogBox;
+
+import javax.swing.JOptionPane;
+
+public class firstclass {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"this is waring massege");
+}
+}
